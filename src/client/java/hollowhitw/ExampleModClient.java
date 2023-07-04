b28a3f4f0e6b45f9dd298a235d573751da9e9c31
HollowCarter <53297773+HollowCarter@users.noreply.github.com>
2023-07-04 22:35:58 +1000

    message logs chat (not MCCI tho ????)

--- src/client/java/hollowhitw/ExampleModClient.java
+++ src/client/java/hollowhitw/ExampleModClient.java
@@ -1,32 +1,44 @@
 package hollowhitw;
 
+import org.jetbrains.annotations.Nullable;
+
 import com.mojang.authlib.GameProfile;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.minecraft.client.gui.hud.InGameHud;
+import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.client.MinecraftClient;
+import net.minecraft.network.message.SignedMessage;
+import net.minecraft.scoreboard.Scoreboard;
 
 public class ExampleModClient implements ClientModInitializer {
 
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
 			PlayerEntity player = client.player;
 
 			if(player != null){
 				GameProfile gameProfile = player.getGameProfile();
                 String playerName = gameProfile.getName();
-                System.out.println("Player name: " + playerName);
+                //System.out.println("Player name: " + playerName);
+
 			}
+		});
+
+
+		ClientReceiveMessageEvents.CHAT.register((message, signedMessage, sender, params,receptionTimestamp) ->
+		{
+			System.out.println("Received chat message: " + message.getString());
+			System.out.println("Received chat signedMessage: " + signedMessage.toString());
+			System.out.println("Received chat sender: " + sender.toString());
+			System.out.println("Received chat params: " + params.toString());
+			System.out.println("Received chat receptionTimestamp: " + receptionTimestamp.toString());
 		});
 	}
 }
 
-//Get Players Name
 //Read Chat
 //Select messages relavnt to code
 //process messages for relvant info
